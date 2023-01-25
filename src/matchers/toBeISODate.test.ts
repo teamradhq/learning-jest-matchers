f205c3654e18cfa02939e502bb8eaa33715bded8
Paul Beynon <paul@beynon.app>
2023-01-25 16:29:26 +1100

    TEST: toBeISODate failure messages

--- src/matchers/toBeISODate.test.ts
+++ src/matchers/toBeISODate.test.ts
@@ -1,4 +1,5 @@
 type IsoDateTestCase = [string | Date];
+type IsoDateMessageTestCase = [string, string | Date];
 
 
 describe('expect.toBeISODate', () => {
@@ -23,6 +24,36 @@ describe('expect.toBeISODate', () => {
     expect.assertions(1);
 
     expect(actual).not.toBeISODate();
+  });
+
+  const messageTestCases: IsoDateMessageTestCase[] = [
+    ['expected 2020-01-01 to be an ISO date', '2020-01-01'],
+    [`expected ${new Date().toString()} to be an ISO date`, new Date()],
+  ];
+
+  it.each(messageTestCases)('should display: "%s" upon failure', (message, actual) => {
+    expect.assertions(2);
+
+    const shouldThrow = () => {
+      expect(actual).toBeISODate();
+    };
+
+    expect(shouldThrow).toThrow(message);
+  });
+
+  const inverseMessageTestCases: IsoDateMessageTestCase[] = [
+    ['expected 2020-01-01T10:00:00.000Z not to be an ISO date', '2020-01-01T10:00:00.000Z'],
+    [`expected ${new Date().toISOString()} not to be an ISO date`, new Date().toISOString()],
+  ];
+
+  it.each(inverseMessageTestCases)('should display: "%s" upon inverse failure', (message, actual) => {
+    expect.assertions(2);
+
+    const shouldThrow = () => {
+      expect(actual).not.toBeISODate();
+    };
+
+    expect(shouldThrow).toThrow(message);
   });
 });
 
